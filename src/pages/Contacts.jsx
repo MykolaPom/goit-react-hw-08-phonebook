43c275d95fb5a135c74ab0cc0874bdb730cf49ea
MykolaPom <mykola.pomazanov@gmail.com>
2023-01-07 17:40:45 +0200

    component hidden

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -24,7 +24,7 @@ export default function ContactsPage() {
       <ContactForm />
       <h2>Contacts</h2>
       <div>{data && 'Phonebook is empty...'}</div>
-      <ContactList name="Contacts" />
+      {/* <ContactList name="Contacts" /> */}
     </MainContainer>
   );
 }
