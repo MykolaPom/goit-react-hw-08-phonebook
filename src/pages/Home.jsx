c90d045ac0882acd02ceb5a2fe99059f4f130dab
MykolaPom <mykola.pomazanov@gmail.com>
2023-01-08 17:01:18 +0200

    adds styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,23 @@
-import {HomeContainer, HomeTitle} from './styled/Home.styled'
+import { HomeContainer, HomeTitle } from './styled/Home.styled'
+import { FaGithubSquare } from 'react-icons/fa';
+import {HomeList} from './styled/Home.styled'
 
 export default function Home() {
   return (
     <HomeContainer>
       <HomeTitle>
-       Welcome to Phonebook application webpage
+        Welcome to Phonebook application webpage
+      </HomeTitle>
+          <h2>Developed by Mykola Pomazanov</h2>
+          <HomeList>
+            <li>
+              <a href="https://github.com/MykolaPom"><FaGithubSquare size={40}/></a>
+            </li>
+          </HomeList>
+
         {/* <span role="img" aria-label="Greeting icon">
           💁‍♀️
         </span> */}
-      </HomeTitle>
     </HomeContainer>
   );
 }
--- src/pages/styled/Home.styled.js
+++ src/pages/styled/Home.styled.js
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 
 export const HomeContainer = styled.div`
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
 `;
@@ -12,4 +13,8 @@ export const HomeTitle = styled.h1`
   margin-top: 100px;
   color: black;
 `;
+
+export const HomeList = styled.ul`
+  list-style: none;
+`
 
